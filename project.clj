78aff8bf292fcf72be275c5f22c62e99628a5531
nitincodery <54881266+nitincodery@users.noreply.github.com>
2025-10-05 18:55:56 +0530

    Delete project.clj

--- project.clj
+++
@@ -1,5 +0,0 @@
-(defproject nitincodery/spy "0.1.0-SNAPSHOT"
-  :description "A small Clojure library for spying on let and fn bindings"
-  :url "https://github.com/nitincodery/spy"
-  :source-paths ["src"]
-  :repl-options {:init-ns tommy-mor.spy})
